809435d622bbb0f47532e841e95d8eac796a6a58
Lele Chen <lelechen@Innopeaks-MacBook-Pro.local>
2021-11-30 22:24:33 -0500

    g

--- evalute.py
+++ evalute.py
@@ -1,6 +1,15 @@
 import os 
 import cv2
 path = '/home/uss00022/lelechen/github/stylegan2-ada-pytorch-lele/out'
+import PIL.Image
+
+# Load mask
+mask = PIL.Image.open("/data/home/uss00022/lelechen/github/lighting/predef/facial_mask_v10.png")
+# mask = PIL.Image.open("/home/uss00022/lelechen/github/lighting/predef/facial_mask_v10.png")
+
+mask = np.array(mask)[500:2500, 1019 : 3019]/ 255.0
+mask = cv2.resize(mask, (G.img_resolution,G.img_resolution))
+mask = np.expand_dims(mask, axis=2)
 
 proj_imgs = []
 for i in os.listdir(path):
@@ -10,6 +19,13 @@ for i in os.listdir(path):
 proj_imgs.sort()
 proj_imgs = proj_imgs[:10]
 for i in proj_imgs:
-    proj =cv2.imread( path + '/' + i )
-    gt = cv2.imread(path +'/' + i.replace('proj', 'target'))
-    print (proj.shape, gt.shape)
+    synth_image =cv2.imread( path + '/' + i )
+    target_pil = PIL.Image.open(path +'/' + i.replace('proj', 'target'))
+
+    image = np.array(target_pil)[500:2500, 1019 : 3019]
+    image = cv2.resize(image, (256,256) ) * mask
+    target_uint8 = image.astype(np.uint8)
+
+    tmp =np.concatenate([target_uint8, synth_image], axis=1)
+    cv2.imwrite('./tmp.png', tmp)
+    break
