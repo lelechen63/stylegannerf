bbfaef670c2463d83beed5dd16d8c0bc7a53c7fb
Lele Chen <lelechen@Innopeaks-MacBook-Pro.local>
2021-12-12 22:18:36 -0500

    g

--- project_debug.py
+++ project_debug.py
@@ -176,37 +176,16 @@ def run_projection(
     image = cv2.resize(image, (G.img_resolution,G.img_resolution) ) * mask
     target_uint8 = image.astype(np.uint8)
 
-    # Optimize projection.
-    start_time = perf_counter()
-    projected_w_steps = project(
-        G,
-        target=torch.tensor(target_uint8.transpose([2, 0, 1]), device=device), # pylint: disable=not-callable
-        num_steps=num_steps,
-        device=device,
-        verbose=True
-    )
-    print (f'Elapsed: {(perf_counter()-start_time):.1f} s')
-
-    # Render debug output: optional video and projected image and W vector.
-    os.makedirs(outdir, exist_ok=True)
-    if save_video:
-        video = imageio.get_writer(f'{outdir}/proj.mp4', mode='I', fps=10, codec='libx264', bitrate='16M')
-        print (f'Saving optimization progress video "{outdir}/proj.mp4"')
-        for projected_w in projected_w_steps:
-            synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
-            synth_image = (synth_image + 1) * (255/2)
-            synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
-            video.append_data(np.concatenate([target_uint8, synth_image], axis=1))
-        video.close()
-
     # Save final projected frame and W vector.
-    target_pil.save(f'{outdir}/target.png')
-    projected_w = projected_w_steps[-1]
+    target_pil.save(f'./target.png')
+    print ( '!!!!!' ,target_fname)
+    # projected_w = 
     synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
     synth_image = (synth_image + 1) * (255/2)
     synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
-    PIL.Image.fromarray(synth_image, 'RGB').save(f'{outdir}/proj.png')
-    np.savez(f'{outdir}/projected_w.npz', w=projected_w.unsqueeze(0).cpu().numpy())
+    tmp = np.concatenate([target_uint8, synth_image], axis=1)
+
+    PIL.Image.fromarray(tmp, 'RGB').save(f'./proj.png')
 
 #----------------------------------------------------------------------------
 
